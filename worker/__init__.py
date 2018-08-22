a103725bb3fc1f2f2f036c31da13133720aef068
soltanoff <piccadillable@gmail.com>
2018-08-22 13:42:31 +0300

    create worker list

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,5 +1,8 @@
 from worker.settings import NEW_TASK_EVENT
 from worker.worker import Worker
 
-WORKER = Worker(NEW_TASK_EVENT)
-WORKER.start()
+WORKER_LIST = [Worker(NEW_TASK_EVENT)]
+
+# map(lambda x: x.start(), WORKER_LIST)
+for x in WORKER_LIST:
+    x.start()
