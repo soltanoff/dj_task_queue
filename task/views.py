eba875b69bbc504f2e539722306232344f2929f3
soltanoff <piccadillable@gmail.com>
2018-12-02 15:30:25 +0300

    move to DRF

--- task/views.py
+++ task/views.py
@@ -1,66 +1,56 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import JsonResponse, Http404
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view, permission_classes
 
 from task.models import TaskModel, Status
 from worker import WORKER_LIST
 from worker.settings import NEW_TASK_EVENT
 
 
-@csrf_exempt
+@api_view(['POST'])
+@permission_classes(permissions.IsAuthenticated)
 def create_task(request):
-    if request.method == 'GET':
-        task = TaskModel(status=Status.IN_QUEUE)
-        task.save()
+    task = TaskModel(status=Status.IN_QUEUE)
+    task.save()
 
-        if NEW_TASK_EVENT.is_set():
-            NEW_TASK_EVENT.clear()
-        NEW_TASK_EVENT.set()
-        return JsonResponse({"task_id": task.pk})
-    else:
-        raise Http404
+    if NEW_TASK_EVENT.is_set():
+        NEW_TASK_EVENT.clear()
+    NEW_TASK_EVENT.set()
+    return JsonResponse({"task_id": task.pk})
 
 
-@csrf_exempt
+@api_view(['GET'])
 def get_info(request, task_id):
-    if request.method == 'GET':
-        try:
-            task = TaskModel.objects.get(pk=task_id)  # type: TaskModel
-            return JsonResponse({
-                'status': task.status.label,
-                'create_time': task.create_time,
-                'start_time': task.start_time,
-                'time_to_execute': str(task.exec_time - task.start_time) if task.exec_time is not None else None,
-            })
-        except ObjectDoesNotExist:
-            return JsonResponse({"message": "Task #%s does not exists" % task_id}, status=400)
-    else:
-        raise Http404
+    try:
+        task = TaskModel.objects.get(pk=task_id)  # type: TaskModel
+        return JsonResponse({
+            'status': task.status.label,
+            'create_time': task.create_time,
+            'start_time': task.start_time,
+            'time_to_execute': str(task.exec_time - task.start_time) if task.exec_time is not None else None,
+        })
+    except ObjectDoesNotExist:
+        return JsonResponse({"message": "Task #%s does not exists" % task_id}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['POST'])
+@permission_classes(permissions.IsAdminUser)
 def start_workers(request):
-    if request.method == 'GET':
-        try:
-            # map(lambda x: x.start(), WORKER_LIST)
-            for x in WORKER_LIST:
-                x.start()
-            return JsonResponse({"message": "workers are running"})
-        except Exception as e:
-            return JsonResponse({"message": str(e)}, status=400)
-    else:
-        raise Http404
+    try:
+        for x in WORKER_LIST:
+            x.start()
+        return JsonResponse({"message": "workers are running"})
+    except Exception as e:
+        return JsonResponse({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['POST'])
+@permission_classes(permissions.IsAdminUser)
 def disable_workers(request):
-    if request.method == 'GET':
-        try:
-            # map(lambda x: x.disable(), WORKER_LIST)
-            for x in WORKER_LIST:
-                x.disable()
-            return JsonResponse({"message": "workers are disabled"})
-        except Exception as e:
-            return JsonResponse({"message": str(e)}, status=400)
-    else:
-        raise Http404
+    try:
+        for x in WORKER_LIST:
+            x.disable()
+        return JsonResponse({"message": "workers are disabled"})
+    except Exception as e:
+        return JsonResponse({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
